54c886587bbcbb5c1dac60098f7c87b07d9798d3
4rror <levlaner@foxmail.com>
2023-02-24 18:23:35 +0800

    iterator pattern sample program

---
+++ iterator-pattern/src/main/java/org/example/Iterator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Iterator {
+    boolean hasNext();
+
+    Object next();
+}
---
+++ iterator-pattern/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+
+        bookShelf.appendBook(new Book("book 1"));
+        bookShelf.appendBook(new Book("book 2"));
+        bookShelf.appendBook(new Book("book 3"));
+        bookShelf.appendBook(new Book("book 4"));
+
+        Iterator it = bookShelf.iterator();
+        while (it.hasNext()) {
+            Book book = (Book) it.next();
+            System.out.println(book.getName());
+        }
+    }
+}
