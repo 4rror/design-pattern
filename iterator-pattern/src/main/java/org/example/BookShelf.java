47ea5cfa19047cf6a0e78426bdf85a3010f67d0e
4rror <levlaner@foxmail.com>
2023-02-24 19:39:32 +0800

    iterator pattern exercise question

--- iterator-pattern/src/main/java/org/example/BookShelf.java
+++ iterator-pattern/src/main/java/org/example/BookShelf.java
@@ -1,24 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
-    private int last = 0;
+    // private Book[] books;
+    private List<Book> books;
+    // private int last = 0;
 
     public BookShelf(int maxSize) {
-        this.books = new Book[maxSize];
+        // this.books = new Book[maxSize];
+        this.books = new ArrayList<>(maxSize);
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        // return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        // this.books[last] = book;
+        this.books.add(book);
+        // last++;
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     public Iterator iterator() {
