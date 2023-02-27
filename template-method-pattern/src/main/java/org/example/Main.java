a2d288ae32c5e88bc818eb94eedac443aa72397d
4rror <levlaner@foxmail.com>
2023-02-27 21:01:22 +0800

    template method pattern sample program

---
+++ template-method-pattern/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+        d1.display();
+        AbstractDisplay d2 = new StringDisplay("Hello, world.");
+        d2.display();
+    }
+}
